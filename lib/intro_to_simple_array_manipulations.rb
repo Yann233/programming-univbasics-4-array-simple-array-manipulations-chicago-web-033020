d1a66a1309f17c9cc55d305706f0d1671ab3f4b7
Yann Xiaoyan Yang <yannxyang233@gmail.com>
2020-03-11 01:18:43 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -67,6 +67,6 @@ end
 
 #This method takes in two parameters, an Array and an Integer
 #and deletes the element at the index of the array that is equal to that integer.
-def using_delete_at(array,integar)
-array.delete[integar]
+def using_delete_at(array,int)
+array.delete(int)
 end
